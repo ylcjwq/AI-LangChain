e70bf75e111e27c62df54e0d1ad168a46efbd967
ylc <756885686@qq.com>
2024-08-11 13:15:34 +0800

    Github loader

---
+++ embedding多数据源加载/webLoader/GithubLoader.js
@@ -0,0 +1,20 @@
+// Github loader 基于某个开源项目构建数据库
+
+//! fetch请求报错 pageContent无法获取数据
+
+import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import ignore from "ignore";
+
+const loader = new GithubRepoLoader(
+  "https://github.com/ylcjwq/resume-template",
+  {
+    branch: "master",
+    recursive: false,
+    unknown: "warn",
+    ignorePaths: ["*.md", "yarn.lock", "*.json"],
+    accessToken: process.env.GITHUB_TOKEN,
+  }
+);
+const docs = await loader.load();
+
+console.log(docs);
